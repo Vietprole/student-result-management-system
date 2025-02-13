90ef7901560b6463466810eeb58775176956f8e5
Ngọc Lam <nglamvn2911@gmail.com>
2025-02-13 10:51:20 +0700

    wip

---
+++ tests/StudentResultManagementSystem.Tests/ServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Student_Result_Management_System.Data;
+
+namespace StudentResultManagementSystem.Tests;
+
+public class ServiceTest{
+    [Fact]
+    public void Test1(){
+        Assert.Equal(1, 1);
+    }
+}
