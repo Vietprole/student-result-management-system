441fd3215c81c20d7efcffd2144b94ac07bd8918
Ngọc Lam <nglamvn2911@gmail.com>
2025-02-13 14:51:49 +0700

    add test framework, use main program for testing

--- tests/StudentResultManagementSystem.Tests/ServiceTest.cs
+++ tests/StudentResultManagementSystem.Tests/ServiceTest.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 using Student_Result_Management_System.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace StudentResultManagementSystem.Tests;
 
-public class ServiceTest{
+public class ServiceTest : IClassFixture<WebApplicationFactory<Program>> {
+
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public ServiceTest(WebApplicationFactory<Program> factory){
+        _factory = factory;
+    }
+
     [Fact]
     public void Test1(){
-        Assert.Equal(1, 1);
+        //TODO: TEST CODE
     }
 }
