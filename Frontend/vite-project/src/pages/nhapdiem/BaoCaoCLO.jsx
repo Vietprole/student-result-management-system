0f43154947234dccd832ae276311d6111f74bf98
Vietprole <phuvietdn@gmail.com>
2025-03-12 15:29:47 +0700

    Remove frontend folder

--- Frontend/vite-project/src/pages/nhapdiem/BaoCaoCLO.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const BaoCaoCLO = () => {
-  const { lopHocPhanId } = useParams();
-  return <div>Báo Cáo CLO Component for Lớp Học Phần ID: {lopHocPhanId}</div>;
-};
-
-export default BaoCaoCLO;
